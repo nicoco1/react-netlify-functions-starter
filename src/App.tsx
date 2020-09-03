fba00b5547ebcccfecfae4972c4d0f7d53c43d23
Nicola Sedda <nicola.sedda@qframe.be>
2020-09-03 18:12:10 +0200

    netlify identity: subscribing identity events

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState, useImperativeHandle } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import netlifyIdentity from 'netlify-identity-widget';
+import { setupMaster } from 'cluster';
 
-netlifyIdentity.init({});
+const App = () => {
+    const [user, setUser] = useState<netlifyIdentity.User | null>();
 
-function App() {
+    useEffect(() => {
+        netlifyIdentity.init({});
+        setUser(netlifyIdentity.currentUser());
+
+        netlifyIdentity.on('login', user => {
+            setUser(user);
+            netlifyIdentity.close();
+        });
+        netlifyIdentity.on('logout', () => {
+            setUser(null);
+            netlifyIdentity.close();
+        });
+    }, []);
+
     return (
         <div className='App'>
             <header className='App-header'>
@@ -14,16 +29,44 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
 
-                <button
-                    onClick={() => {
-                        netlifyIdentity.open();
-                    }}
-                >
-                    Log in
-                </button>
+                {!user && <p>Please login!</p>}
+                {user && (
+                    <p>
+                        Hello, <strong>{user.user_metadata.full_name}</strong>
+                    </p>
+                )}
+
+                {!user && (
+                    <button
+                        onClick={() => {
+                            netlifyIdentity.open();
+                        }}
+                    >
+                        Log in
+                    </button>
+                )}
+
+                {user && (
+                    <button
+                        onClick={() => {
+                            netlifyIdentity.logout();
+                        }}
+                    >
+                        Log out
+                    </button>
+                )}
+                {user && (
+                    <button
+                        onClick={() => {
+                            console.log(netlifyIdentity.currentUser());
+                        }}
+                    >
+                        Me
+                    </button>
+                )}
             </header>
         </div>
     );
-}
+};
 
 export default App;
