99b483ffbc3490c98757bc7ce9e66b437c7419b1
Nicola Sedda <nicola.sedda@qframe.be>
2020-09-04 22:37:06 +0200

    fix netlify deploy

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
-import { UserContext } from './components/UserContext';
+import React from 'react';
+// import { UserContext } from './components/UserContext';
 
 const Home = () => {
-    //const user = useContext(UserContext);
+    //const user = React.useContext(UserContext);
 
     return <h2>Welcome home {'user' || '???'}!</h2>;
 };
